4bfdc1d4afb3792cd227e26f1d2473f6c72a7403
ammar <ammarraneez@gmail.com>
2022-09-17 12:59:06 +0530

    Add list of tabs

---
+++ api/requests.js
@@ -0,0 +1,37 @@
+const requests = {
+  fetchTrending: {
+    title: 'Trending',
+  },
+  fetchTopRated: {
+    title: 'Top Rated',
+  },
+  fetchActionMovies: {
+    title: 'Action',
+  },
+  fetchComedyMovies: {
+    title: 'Comedy',
+  },
+  fetchHorroeMovies: {
+    title: 'Horror',
+  },
+  fetchRomanceMovies: {
+    title: 'Romance',
+  },
+  fetchMystery: {
+    title: 'Mystery',
+  },
+  fetchSciFi: {
+    title: 'Sci-Fi',
+  },
+  fetchWestern: {
+    title: 'Western',
+  },
+  fetchAnimation: {
+    title: 'Animation',
+  },
+  fetchTv: {
+    title: 'TV Movie',
+  },
+}
+
+export default requests;
